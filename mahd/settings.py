d154c8ffd3808184c6c4898874f994bb23e5f103
sadegh <mhmdsadqd@gmail.com>
2021-05-29 20:40:28 +0430

    first commit and add templates and static --sadegh

--- mahd/settings.py
+++ mahd/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+# STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'mahd/static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
