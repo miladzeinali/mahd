a1c658e172894b4d174debdbb653fe5347653b3f
miladzeinali <58866441+miladzeinali@users.noreply.github.com>
2021-05-30 09:13:36 +0430

    alak

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class Userprofileadmin(admin.ModelAdmin):
+    list_display = ('id','name','last_name','home_phone','father_name')
+    search_fields = ('name','nin','home_phone')
+
+
+admin.site.register(Userprofile,Userprofileadmin)
