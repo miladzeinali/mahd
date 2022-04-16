08035340b36a08194a7c422f28cb1b9d3c6f153c
milad.zeinalimce@yahoo.com <milad.zeinalimce@yahoo.com>
2022-04-16 18:31:24 +0430

    add some field to the form --sadegh

--- web/models.py
+++ web/models.py
@@ -6,7 +6,12 @@ class Userprofile(models.Model):
     birthday = models.CharField(max_length=20)
     birth_place = models.CharField(max_length=35)
     create_card_place = models.CharField(max_length=35)
-    serial_card = models.CharField(max_length=35)
+    Sericard = models.CharField(max_length=35,null=True,blank=True)
+    Serialcard = models.CharField(max_length=35,null=True,blank=True)
+    Fatherseri = models.CharField(max_length=35,null=True,blank=True)
+    Fatherserial = models.CharField(max_length=35,null=True,blank=True)
+    Motherseri = models.CharField(max_length=35,null=True,blank=True)
+    Motherserial = models.CharField(max_length=35,null=True,blank=True)
     nin = models.CharField(max_length=12)
     home_phone = models.CharField(max_length=35)
     home_code = models.CharField(max_length=35)
@@ -16,7 +21,7 @@ class Userprofile(models.Model):
     father_name_lastname = models.CharField(max_length=50)
     father_father_name = models.CharField(max_length=50)
     father_nin = models.CharField(max_length=12)
-    father_serial_card = models.CharField(max_length=20)
+    # father_serial_card = models.CharField(max_length=20)
     # father_create_card_place = models.CharField(max_length=35)
     father_education = models.CharField(max_length=35)
     father_phone = models.CharField(max_length=35)
@@ -25,7 +30,7 @@ class Userprofile(models.Model):
     mother_name = models.CharField(max_length=50)
     mother_father_name = models.CharField(max_length=50)
     mother_nin = models.CharField(max_length=12)
-    mother_serial_card = models.CharField(max_length=20)
+    # mother_serial_card = models.CharField(max_length=20)
     mother_create_card_place = models.CharField(max_length=35)
     mother_education = models.CharField(max_length=35)
     mother_phone = models.CharField(max_length=35)
@@ -38,5 +43,15 @@ class Userprofile(models.Model):
         ('پیش دبستانی یگانه','پیش دبستانی یگانه')
     )
     e_place = models.CharField(choices=e_place_choices,max_length=30,default='مهد دردانه')
+    Hand = models.CharField(max_length=35,null=True,blank=True)
+    Talagh = models.CharField(max_length=35,null=True,blank=True)
+    Shahid = models.CharField(max_length=35,null=True,blank=True)
+    Bimeh = models.CharField(max_length=35,null=True,blank=True)
+    Farhangi = models.CharField(max_length=35,null=True,blank=True)
+    Children = models.CharField(max_length=35,null=True,blank=True)
+    Child = models.CharField(max_length=35,null=True,blank=True)
+
+    
+
 
 
--- web/views.py
+++ web/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Userprofile
 from django.contrib import messages
 from .SmsHandler import SmsHandler
 from django.views.decorators.csrf import csrf_exempt
+
 sms = SmsHandler()
+
+
 def home(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
 
 @csrf_exempt
 def SignUp(request):
@@ -16,16 +20,30 @@ def SignUp(request):
         birth_place = request.POST['birthdayplace']
         nin = request.POST['nin']
         create_card_place = request.POST['createcardplace']
-        serial_card = request.POST['serialcard']
+        sericard = request.POST['sericard']
+        serialcard = request.POST['serialcard']
+        fatherseri = request.POST['fatherseri']
+        fatherserial = request.POST['fatherserial']
+        motherseri = request.POST['motherseri']
+        motherserial = request.POST['motherserial']
         home_phone = request.POST['homephone']
         home_code = request.POST['homecode']
         address = request.POST['address']
         city = request.POST['city']
         province = request.POST['province']
+
+        hand = request.POST['hand']
+        talagh = request.POST['talagh']
+        bimeh = request.POST['bimeh']
+        shahid = request.POST['shahid']
+        farhangi = request.POST['farhangi']
+        children = request.POST['children']
+        child = request.POST['child']
+
         father_name_lastname = request.POST['fathername']
         father_father_name = request.POST['fatherfathername']
         father_nin = request.POST['fathernin']
-        father_serial_card = request.POST['fatherserial']
+        # father_serial_card = request.POST['fatherserial']
         father_education = request.POST['fathereducation']
         father_phone = request.POST['fatherphone']
         father_work = request.POST['fatherwork']
@@ -33,7 +51,7 @@ def SignUp(request):
         mother_name = request.POST['mothername']
         mother_father_name = request.POST['motherfathername']
         mother_nin = request.POST['mothernin']
-        mother_serial_card = request.POST['notherserial']
+        # mother_serial_card = request.POST['motherserial']
         mother_education = request.POST['mothereducation']
         mother_phone = request.POST['motherphone']
         mother_work = request.POST['motherwork']
@@ -42,35 +60,43 @@ def SignUp(request):
         eplace = request.POST['eplace']
         try:
             Userprofile.objects.get(nin=nin)
-            messages.error(request,{'کد ملی تکراری است.'})
-            return render(request,'admission.html')
+            messages.error(request, {'کد ملی تکراری است.'})
+            return render(request, 'admission.html')
         except:
-            Userprofile.objects.create(name=name,father=father,birthday=birthday,birth_place=birth_place,nin=nin,
-                                       create_card_place=create_card_place,serial_card=serial_card,home_phone=home_phone,home_code=home_code,
-                                       address=address,city=city,province=province,father_name_lastname=father_name_lastname,father_father_name=father_father_name,
-                                       father_nin=father_nin,father_serial_card=father_serial_card,father_education=father_education,father_phone=father_phone,
-                                       father_work=father_work,father_birth=father_birth,mother_name=mother_name,mother_father_name=mother_father_name,mother_nin=mother_nin,
-                                       mother_serial_card=mother_serial_card,mother_education=mother_education,mother_phone=mother_phone,mother_work=mother_work,
-                                       mother_birth=mother_birth,telegram_phone=telegram_phone,e_place=eplace)
-            messages.success(request,{'اطلاعات شما با موفقیت ثبت شد .'})
+            Userprofile.objects.create(name=name, father=father, birthday=birthday
+                                       , birth_place=birth_place, nin=nin, create_card_place=create_card_place,
+                                       Sericard=sericard, Serialcard=serialcard, Fatherseri=fatherseri,
+                                       Fatherserial=fatherserial, Motherseri=motherseri, Motherserial=motherserial,
+                                       Hand=hand, Talagh=talagh, Bimeh=bimeh, Shahid=shahid, Farhangi=farhangi,
+                                       Children=children, Child=child, home_phone=home_phone, home_code=home_code,
+                                       address=address, city=city, province=province,
+                                       father_name_lastname=father_name_lastname, father_father_name=father_father_name,
+                                       father_nin=father_nin, father_education=father_education,
+                                       father_phone=father_phone, father_work=father_work, father_birth=father_birth,
+                                       mother_name=mother_name, mother_father_name=mother_father_name,
+                                       mother_nin=mother_nin, mother_education=mother_education,
+                                       mother_phone=mother_phone, mother_work=mother_work, mother_birth=mother_birth,
+                                       telegram_phone=telegram_phone, e_place=eplace)
+            messages.success(request, {'اطلاعات شما با موفقیت ثبت شد .'})
             token = sms.get_token()
-            sms.send_register_report(name,telegram_phone,token)
-            return render(request,'success.html')
+            sms.send_register_report(name, telegram_phone, token)
+            return render(request, 'success.html')
     else:
         pass
-    return render(request,'admission.html')
-    
+    return render(request, 'admission.html')
+
+
 def Userprofiles(request):
     try:
-        userprofiles = Userprofile.objects.all()
-        return render(request,'userprofile.html',{'userprofiles':userprofiles})
+        userprofiles = Userprofile.objects.all().order_by('-id')
+        return render(request, 'userprofile.html', {'userprofiles': userprofiles})
     except:
-        return render(request,'index.html')
-        
-    
-def UserDetail(request,id):
+        return render(request, 'index.html')
+
+
+def UserDetail(request, id):
     try:
         userprofile = Userprofile.objects.get(id=id)
-        return render(request,'userdetail.html',{'userprofile':userprofile})
+        return render(request, 'userdetail.html', {'userprofile': userprofile})
     except:
-        return render(request,'index.html')
+        return render(request, 'index.html')
