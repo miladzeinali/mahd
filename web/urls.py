a4c2d8d219b06c07c7f69e05cc3c1c664aa647e3
shiateam <mhmdsadqd@gmail.com>
2022-04-18 22:59:16 +0430

    secund commit for add payment to project --sadegh

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import SignUp,home
+from .views import SignUp, Userprofiles,home,UserDetail
 
 app_name = 'web'
 urlpatterns = [
     path('signup/',SignUp,name='signup'),
     path('',home,name='home'),
+    path('userprofiles/',Userprofiles,name='userprofile'),
+    path('userdetail/<int:id>/',UserDetail,name='detail'),
 ]
